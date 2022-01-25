4b3d5f3248bff041d8686ceaf292f162fd05e4aa
Melker Widén <melker.widen@gmail.com>
2022-01-25 20:26:18 +0100

    ...

--- testing/test.py
+++ testing/test.py
@@ -1,2 +1,12 @@
+import random
+random.seed(1)
 
-print(max([1,99,3]))
+def generate_grid():
+
+    chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', '.']
+    chars = random.sample(chars, 16)
+    result = []
+    for i in range(4):
+        result.append(chars[i * 4: i * 4 + 4])
+
+    return result
