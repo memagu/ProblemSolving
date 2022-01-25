0ea5ebef503c90169fdc4b47c36a47f42b7c7826
Melker Widén <melker.widen@gmail.com>
2022-01-25 20:25:22 +0100

    Solution Added

---
+++ Kattis/Python/A New Alphabet.py
@@ -0,0 +1,2 @@
+dictionary_lowercase = {"a": "@", "b": "8", "c": "(", "d": "|)", "e": "3", "f": "#", "g": "6", "h": "[-]", "i": "|", "j": "_|", "k": "|<", "l": "1", "m": "[]\\/[]", "n": "[]\\[]", "o": "0", "p": "|D", "q": "(,)", "r": "|Z", "s": "$", "t": "']['", "u": "|_|", "v": "\\/", "w": "\\/\\/", "x": "}{", "y": "`/", "z": "2"}
+print("".join(char if char not in dictionary_lowercase else dictionary_lowercase[char] for char in input().lower()))
