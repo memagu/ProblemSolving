b753216389805ef0aca4932caafa02f1e01ef801
Melker Widén <85902964+memagu@users.noreply.github.com>
2022-02-15 18:07:40 +0100

    Delete testing directory

--- testing/test2.py
+++
@@ -1,7 +0,0 @@
-with open("test.txt", "w") as f:
-    _ = input().strip()
-    ltr = input().strip()
-    rtl = input().strip()
-    shift = int(input().strip())
-
-    f.write(f"{_} {ltr} {rtl} {shift}")
