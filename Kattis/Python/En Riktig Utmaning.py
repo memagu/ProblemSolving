75781b87cacc2f464036320a6fe07b021be6761f
Melker Widén <melker.widen@gmail.com>
2022-05-27 02:42:39 +0200

    problems added

--- Kattis/Python/En Riktig Utmaning.py
+++ Kattis/Python/En Riktig Utmaning.py
@@ -1,7 +1,10 @@
-import math
+# import math
+#
+# a = input()
+#
+# o = math.sqrt(int(a)) * 4
+#
+# print(float(o))
 
-a = input()
-
-o = math.sqrt(int(a)) * 4
-
-print(float(o))
+from math import sqrt
+print(sqrt(int(input())) * 4)
