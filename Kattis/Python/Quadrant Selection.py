b02a665442648fd5d6f9117ca0fe12de566a3e62
Melker Widén <melker.widen@gmail.com>
2022-08-29 20:23:34 +0200

    problems added

--- Kattis/Python/Quadrant Selection.py
+++ Kattis/Python/Quadrant Selection.py
@@ -1,10 +1,22 @@
-x, y = input(), input()
+x, y = map(int, (input(), input()))
 
-if int(x) > 0 and int(y) > 0:
-    print(1)
-elif int(x) > 0 and int(y) < 0:
-    print(4)
-elif int(x) < 0 and int(y) < 0:
-    print(3)
+# if int(x) > 0 and int(y) > 0:
+#     print(1)
+# elif int(x) > 0 and int(y) < 0:
+#     print(4)
+# elif int(x) < 0 and int(y) < 0:
+#     print(3)
+# else:
+#     print(2)
+
+
+if x > 0:
+    if y > 0:
+        print(1)
+    else:
+        print(4)
 else:
-    print(2)
+    if y > 0:
+        print(2)
+    else:
+        print(3)
