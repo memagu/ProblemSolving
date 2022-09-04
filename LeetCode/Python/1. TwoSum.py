bd3cbdd3783f5db76b70bceaa0614384dedf684a
Melker Widén <melker.widen@gmail.com>
2022-09-04 19:43:21 +0200

    problems added

--- LeetCode/Python/1. TwoSum.py
+++ LeetCode/Python/1. TwoSum.py
@@ -1,13 +1,32 @@
-indices = {}
-for i, num in enumerate(nums):
-    indices[num] = i
+from typing import List
 
-for i, element in enumerate(nums):
-    difference = target - element
-    try:
-        j = indices[difference]
-        if j == i:
-            continue
-        return [i, j]
-    except KeyError:
-        continue
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         indices = {}
+#         for i, num in enumerate(nums):
+#             indices[num] = i
+#
+#         for i, element in enumerate(nums):
+#             difference = target - element
+#             try:
+#                 j = indices[difference]
+#                 if j == i:
+#                     continue
+#                 return [i, j]
+#             except KeyError:
+#                 continue
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {num: i for i, num in enumerate(nums)}
+
+        for i, num in enumerate(nums):
+            num2 = target - num
+            if num2 not in hash_map:
+                continue
+
+            j = hash_map[num2]
+
+            if i != j:
+                return [i, j]
