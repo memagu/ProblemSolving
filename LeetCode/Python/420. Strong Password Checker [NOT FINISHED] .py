0546490b977bd657eeb9e436873652371e8f5720
Melker Widén <melker.widen@gmail.com>
2022-09-05 00:38:39 +0200

    solutions added

--- LeetCode/Python/420. Strong Password Checker [NOT FINISHED] .py
+++ LeetCode/Python/420. Strong Password Checker [NOT FINISHED] .py
@@ -7,55 +7,128 @@ It does not contain three repeating characters in a row (i.e., "...aaa..." is we
 assuming other conditions are met).
 """
 import string
+from collections import defaultdict
+
+
+# class Solution:
+#     def strongPasswordChecker(self, password: str) -> int:
+#         edits_required = 0
+#         lower = True
+#         upper = True
+#
+#         i = 0
+#         while i < len(password) - 2:
+#             if password[i] == password[i + 1] == password[i + 2]:
+#                 edits_required += 1
+#                 i += 3
+#                 continue
+#             i += 1
+#
+#         removal_edits = max(len(password) - 20, 0)
+#         if len(password) > 20:
+#             edits_required = max(removal_edits - edits_required, edits_required)
+#
+#         elif len(password) < 6:
+#             edits_required += 6 - len(password) - edits_required
+#
+#         if not any(item in string.ascii_lowercase for item in password):
+#             lower = False
+#             if not edits_required - removal_edits:
+#                 edits_required += 1
+#
+#         if not any(item in string.ascii_uppercase for item in password):
+#             upper = False
+#             if not edits_required - (not lower) - removal_edits:
+#                 edits_required += 1
+#
+#         if not any(item in string.digits for item in password):
+#             if not edits_required - (not lower) - (not upper) - removal_edits:
+#                 edits_required += 1
+#
+#         return edits_required
 
 
 class Solution:
     def strongPasswordChecker(self, password: str) -> int:
-        edits_required = 0
-        lower = True
-        upper = True
+        upper = 1
+        lower = 1
+        digit = 1
+        to_add = 0
+        to_delete = 0
+        triplets = 0
+        used = 0
 
-        i = 0
-        while i < len(password) - 2:
-            if password[i] == password[i + 1] == password[i + 2]:
-                edits_required += 1
-                i += 3
-                continue
-            i += 1
+        groups = [0]
+        group_index = 0
 
-        removal_edits = max(len(password) - 20, 0)
+        last_char = password[0]
+        for char in password:
+            if char == last_char:
+                groups[group_index] += 1
+            else:
+                groups.append(1)
+                group_index += 1
+            if char.islower():
+                lower = 0
+            elif char.isupper():
+                upper = 0
+            elif char in "0123456789":
+                digit = 0
+            last_char = char
+
+        uld = upper + lower + digit
+
+        for group in groups:
+            triplets += group // 3
+
         if len(password) > 20:
-            edits_required = max(removal_edits - edits_required, edits_required)
+            to_delete = len(password) - 20
 
         elif len(password) < 6:
-            edits_required += 6 - len(password) - edits_required
+            to_add = 6 - len(password)
 
-        if not any(item in string.ascii_lowercase for item in password):
-            lower = False
-            if not edits_required - removal_edits:
-                edits_required += 1
+        if to_add:
+            if to_add > uld:
+                used += uld
+                to_add, uld = to_add - uld, 0
+            else:
+                used += to_add
+                uld, to_add = uld - to_add, 0
 
-        if not any(item in string.ascii_uppercase for item in password):
-            upper = False
-            if not edits_required - (not lower) - removal_edits:
-                edits_required += 1
+            if to_add > triplets:
+                used += triplets
+                to_add, triplets = to_add - triplets, 0
+            else:
+                used += to_add
+                triplets, to_add = triplets - to_add, 0
 
-        if not any(item in string.digits for item in password):
-            if not edits_required - (not lower) - (not upper) - removal_edits:
-                edits_required += 1
+        if to_delete:
+            if to_delete > triplets:
+                used += triplets
+                to_delete, triplets = to_delete - triplets, 0
+            else:
+                used += to_delete
+                triplets, to_delete = triplets - to_delete, 0
 
-        return edits_required
+        if triplets:
+            if triplets > uld:
+                used += uld
+                triplets, uld = triplets - uld, 0
+            else:
+                used += triplets
+                uld, triplets = uld - triplets, 0
+
+
+        print(uld, to_add, to_delete, triplets)
+
+        return used + uld + to_add + to_delete + triplets
 
 
 if __name__ == "__main__":
-    print(Solution().strongPasswordChecker("a"))
-    print(Solution().strongPasswordChecker("aA1"))
-    print(Solution().strongPasswordChecker("1337C0d3"))
-    print(Solution().strongPasswordChecker("aaa111"))
-    print(Solution().strongPasswordChecker("aaaB1"))
-    print(Solution().strongPasswordChecker("ABABABABABABABABABAB1"))
-    print(Solution().strongPasswordChecker("bbaaaaaaaaaaaaaaacccccc"))
-    "bb faa faa faa faa faa fcc ccc"
-    "bb  aa faa 8aa 8aa faa  cc  Fc"
-    "bbaaaaaaaaaaaaaaacccccc"
-    print(len("bbaaaaaaaaaaaaaaacccccc")) #7-3
+    print(Solution().strongPasswordChecker("a"))  # 5
+    print(Solution().strongPasswordChecker("aA1"))  # 3
+    print(Solution().strongPasswordChecker("1337C0d3"))  # 0
+    print(Solution().strongPasswordChecker("aaa111"))  # 2
+    print(Solution().strongPasswordChecker("aaaB1"))  # 1
+    print(Solution().strongPasswordChecker("ABABABABABABABABABAB1"))  # 2
+    print(Solution().strongPasswordChecker("bbaaaaaaaaaaaaaaacccccc"))  # 8
