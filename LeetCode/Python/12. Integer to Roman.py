e96ad797e6c8b9e18492dcfb0479ef65f9488426
Melker Widén <melker.widen@gmail.com>
2022-09-14 16:33:27 +0200

    solution added

---
+++ LeetCode/Python/12. Integer to Roman.py
@@ -0,0 +1,71 @@
+# class Solution:
+#     def intToRoman(self, num: int) -> str:
+#         num = str(num)
+#
+#         integer_to_roman = {1: "I", 5: "V", 10: "X", 50: "L", 100: "C", 500: "D", 1000: "M"}
+#         index_to_builders = ((1, 5), (10, 50), (100, 500), (1000, 1000))
+#
+#         result = ""
+#         for i, part in enumerate(num):
+#             i = (len(num) - 1 - i)
+#             part = int(part) * 10 ** i
+#
+#             if part in integer_to_roman:
+#                 result += integer_to_roman[part]
+#                 continue
+#
+#             if i > 2:
+#                 result += integer_to_roman[1000] * (part // 1000)
+#                 continue
+#
+#             if part > index_to_builders[i][1]:
+#                 if part == index_to_builders[i+1][0] - index_to_builders[i][0]:
+#                     result += integer_to_roman[index_to_builders[i][0]] + integer_to_roman[index_to_builders[i+1][0]]
+#                     continue
+#                 result += integer_to_roman[index_to_builders[i][1]] + integer_to_roman[index_to_builders[i][0]] * ((part - index_to_builders[i][1]) // index_to_builders[i][0])
+#                 continue
+#
+#             if part == index_to_builders[i][1] - index_to_builders[i][0]:
+#                 result += integer_to_roman[index_to_builders[i][0]] + integer_to_roman[index_to_builders[i][1]]
+#                 continue
+#
+#             result += integer_to_roman[index_to_builders[i][0]] * (part // index_to_builders[i][0])
+#
+#         return result
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        conversion = ((1000, 'M'),
+                  (900, 'CM'),
+                  (500, 'D'),
+                  (400, 'CD'),
+                  (100, 'C'),
+                  (90, 'XC'),
+                  (50, 'L'),
+                  (40, 'XL'),
+                  (10, 'X'),
+                  (9, 'IX'),
+                  (5, 'V'),
+                  (4, 'IV'),
+                  (1, 'I'))
+
+        result = ""
+
+        i = 0
+        while num:
+            decimal, roman = conversion[i]
+            if num >= decimal:
+                num -= decimal
+                result += roman
+            else:
+                i += 1
+
+        return result
+
+
+
+if __name__ == "__main__":
+    print(Solution().intToRoman(58))
+    print(Solution().intToRoman(1994))
+    print(Solution().intToRoman(20))
+    print(Solution().intToRoman(60))
--- LeetCode/Python/36. Valid Sudoku.py
+++ LeetCode/Python/36. Valid Sudoku.py
@@ -1,29 +1,49 @@
 from typing import List
 
-from collections import defaultdict
+from collections import Counter
 
 
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for row in board:
-            row_counter = defaultdict(int)
-            for num in row:
-                if num == ".":
-                    continue
-                if row_counter[num]:
-                    return False
-                row_counter[num] += 1
+    def validate_list(self, l: List[str]):
+        l = [char for char in l if char != "."]
+        return len(l) == len(set(l))
 
-            for column in row:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for i in range(9):
+            row = board[i]
+            col = [board[row][i] for row in range(9)]
+            if not (self.validate_list(row) and self.validate_list(col)):
+                return False
+
+        for row in range(1, 8, 3):
+            for col in range(1, 8, 3):
+                box = []
+                for i in range(-1, 2):
+                    for j in range(-1, 2):
+                        box.append(board[row - i][col - j])
+                if not self.validate_list(box):
+                    return False
+
+        return True
 
 
 if __name__ == "__main__":
-    print(Solution().isValidSudoku([["5", "3", ".", ".", "7", ".", ".", ".", "."]
-                                       , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
-                                       , [".", "9", "8", ".", ".", ".", ".", "6", "."]
-                                       , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
-                                       , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
-                                       , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
-                                       , [".", "6", ".", ".", ".", ".", "2", "8", "."]
-                                       , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
-                                       , [".", ".", ".", ".", "8", ".", ".", "7", "9"]]))
+    print(Solution().isValidSudoku([["5", "3", ".", ".", "7", ".", ".", ".", "."],
+                                    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+                                    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+                                    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+                                    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+                                    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+                                    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+                                    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+                                    [".", ".", ".", ".", "8", ".", ".", "7", "9"]]))
+
+    print(Solution().isValidSudoku([[".", ".", "4", ".", ".", ".", "6", "3", "."],
+                                    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+                                    ["5", ".", ".", ".", ".", ".", ".", "9", "."],
+                                    [".", ".", ".", "5", "6", ".", ".", ".", "."],
+                                    ["4", ".", "3", ".", ".", ".", ".", ".", "1"],
+                                    [".", ".", ".", "7", ".", ".", ".", ".", "."],
+                                    [".", ".", ".", "5", ".", ".", ".", ".", "."],
+                                    [".", ".", ".", ".", ".", ".", ".", ".", "."],
+                                    [".", ".", ".", ".", ".", ".", ".", ".", "."]]))
