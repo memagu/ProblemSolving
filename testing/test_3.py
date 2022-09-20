f66be4c480549b892fe9ed66115f3b2405ae2037
Melker Widén <melker.widen@gmail.com>
2022-09-20 23:25:17 +0200

    solution added

---
+++ testing/test_3.py
@@ -0,0 +1,8 @@
+# result = 0
+#
+# for row in range(rows):
+#     for col in range(cols):
+#         if len({down[col], left[row], up[col], right[row]}) == 3:
+#             result += 1
+#
+# print(result)
