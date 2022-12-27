e4855ad74a0357d99de37c60a692858e72fb4cfd
Melker Widén <melker.widen@gmail.com>
2022-12-27 14:40:52 +0100

    misc

--- testing/test_3.py
+++ testing/test_3.py
@@ -1,5 +1,17 @@
-a = 10
-b = 9
-c = 0
+def two_sum(nums, target):
+    hash_map = {num: i for i, num in enumerate(nums)}
 
-print(a / b / c)
+    for i, num in enumerate(nums):
+        difference = target - num
+
+        if difference in hash_map:
+            return [i, hash_map[difference]]
+
+    return None
+
+# nums = [1, 11, 3, 2, 5, 4]
+# nums2 = [3, 3]
+# target = 15
+# print(two_sum(nums, target))
+# print(two_sum(nums2, 6))
+
