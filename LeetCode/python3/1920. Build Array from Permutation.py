1a6f88603493180fa640eb80d5f437ff859ae370
Melker Widén <melker.widen@gmail.com>
2023-01-12 07:37:06 +0100

    solution updated

--- LeetCode/python3/1920. Build Array from Permutation.py
+++ LeetCode/python3/1920. Build Array from Permutation.py
@@ -1,4 +1,12 @@
+from typing import List
+
+
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
         ans = [nums[nums[i]] for i in range(len(nums))]
         return ans
+
+
+if __name__ == '__main__':
+    print(Solution().buildArray([0, 2, 1, 5, 3, 4]))  # [0,1,2,4,5,3]
+    print(Solution().buildArray([5, 0, 1, 2, 3, 4]))  # [4,5,0,1,2,3]
