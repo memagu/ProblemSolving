e1ff26d9ab3d488bbcc62a04e4c5c023abb65a9f
Melker Widén <melker.widen@gmail.com>
2023-02-08 10:48:29 +0100

    solution updated

--- LeetCode/python3/14. Longest Common Prefix.py
+++ LeetCode/python3/14. Longest Common Prefix.py
@@ -1,3 +1,8 @@
+from typing import Dict, List, Optional, Tuple
+
+"""
+# Old solution
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         prefix = strs[0]
@@ -13,3 +18,19 @@ class Solution:
                     break
 
         return prefix
+"""
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        for i, char in enumerate(strs[0]):
+            for j in range(1, len(strs)):
+                if len(strs[j]) - 1 < i or strs[j][i] != char:
+                    return strs[0][:i]
+
+        return strs[0]
+
+
+if __name__ == "__main__":
+    print(Solution().longestCommonPrefix(["flower", "flow", "flight"]))
+    print(Solution().longestCommonPrefix(["dog", "racecar", "car"]))
