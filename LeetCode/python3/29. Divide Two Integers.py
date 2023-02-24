39634a08f76f0d7bcddb0c139812678fb0c09264
Melker Widén <melker.widen@gmail.com>
2023-02-24 22:34:01 +0100

    solution updated

--- LeetCode/python3/29. Divide Two Integers.py
+++ LeetCode/python3/29. Divide Two Integers.py
@@ -3,9 +3,11 @@ from typing import Dict, List, Optional, Tuple
 
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        return max(-2 ** 31, min(2 ** 31 - 1, int(dividend / divisor)))
+        return max(min(len(range(0, abs(dividend) - abs(divisor) + 1, abs(divisor))) * (-1 if dividend >= 0 > divisor or dividend < 0 <= divisor else 1), 2 ** 31 - 1), -2 ** 31)
 
 
 if __name__ == "__main__":
     print(Solution().divide(10, 3))
     print(Solution().divide(7, -3))
+    print(Solution().divide(1, 1))
+    print(Solution().divide(2147483647, 1))
