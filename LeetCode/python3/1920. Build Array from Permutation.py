c4db93fd6a0e26a074162f23fdae6044fa00ef4e
Melker Widén <melker.widen@gmail.com>
2023-03-08 16:33:52 +0100

    solution updated

--- LeetCode/python3/1920. Build Array from Permutation.py
+++ LeetCode/python3/1920. Build Array from Permutation.py
@@ -3,10 +3,9 @@ from typing import List
 
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        ans = [nums[nums[i]] for i in range(len(nums))]
-        return ans
+        return [nums[nums[i]] for i in range(len(nums))]
 
 
 if __name__ == '__main__':
-    print(Solution().buildArray([0, 2, 1, 5, 3, 4]))  # [0,1,2,4,5,3]
-    print(Solution().buildArray([5, 0, 1, 2, 3, 4]))  # [4,5,0,1,2,3]
+    print(Solution().buildArray([0, 2, 1, 5, 3, 4]))  # -> [0, 1, 2, 4, 5, 3]
+    print(Solution().buildArray([5, 0, 1, 2, 3, 4]))  # -> [4, 5, 0, 1, 2, 3]
