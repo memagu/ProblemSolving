23c319df6a2493dfe4edd4f2decbccc3efbdd2c4
Melker Widén <melker.widen@gmail.com>
2023-06-15 15:33:57 +0200

    tool updated

--- LeetCode/leetcode_file_generator.py
+++ LeetCode/leetcode_file_generator.py
@@ -1,124 +1,153 @@
 import os
+import re
+from pathlib import Path
+
 import requests
-from typing import Dict, List, Tuple
+
+PROGRAMMING_LANGUAGES = {
+    "c++": "cpp",
+    "cpp": "cpp",
+    "java": "java",
+    "python": "python",
+    "py": "python",
+    "python3": "python3",
+    "py3": "python3",
+    "c": "c",
+    "c#": "csharp",
+    "cs": "csharp",
+    "csharp": "csharp",
+    "javascript": "javascript",
+    "js": "javascript",
+    "ruby": "ruby",
+    "rb": "ruby",
+    "swift": "swift",
+    "go": "golang",
+    "golang": "golang",
+    "scala": "scala",
+    "kotlin": "kotlin",
+    "kt": "kotlin",
+    "rust": "rust",
+    "rs": "rust",
+    "php": "php",
+    "typescript": "typescript",
+    "ts": "typescript",
+    "racket": "racket",
+    "rkt": "racket",
+    "dart": "dart"
+}
+LANGUAGE_FILE_EXTENSIONS = {
+    "cpp": ".cpp",
+    "java": ".java",
+    "python": ".py",
+    "python3": ".py",
+    "c": ".c",
+    "csharp": ".cs",
+    "javascript": ".js",
+    "ruby": ".rb",
+    "swift": ".swift",
+    "golang": ".go",
+    "scala": ".scala",
+    "kotlin": ".kt",
+    "rust": ".rs",
+    "php": ".php",
+    "typescript": ".ts",
+    "racket": ".rkt",
+    "dart": ".dart"
+}
+
+URL = "https://leetcode.com/graphql/"
+GQL_QUERY = """
+            query consolePanelConfig($titleSlug: String!) {
+                question(titleSlug: $titleSlug) {
+                    questionTitle
+                    questionFrontendId
+                    exampleTestcaseList
+                    codeSnippets {
+                        lang langSlug code
+                    }
+                }
+            }
+            """
 
 
-def get_problem_data(url: str) -> Tuple[int, str, List[str], List[Dict[str, str]]]:
-    title_slug = url.split('/')[-2]
+def get_problem_data(url: str) -> dict[str]:
+    return requests.get(
+        URL,
+        json={
+            "query": GQL_QUERY,
+            "variables": {"titleSlug": re.search(r"(?<=problems/)[^\s/]+", url).group()}
+        }
+    ).json()["data"]["question"]
 
-    json_query_info = {
-        "query": "query consolePanelConfig($titleSlug: String!) { question(titleSlug: $titleSlug) { questionId questionFrontendId questionTitle enableDebugger enableRunCode enableSubmit enableTestMode exampleTestcaseList metaData } }",
-        "variables": {"titleSlug": title_slug}}
 
-    json_query_code_snippets = {
-        "query": "query questionEditorData($titleSlug: String!) { question(titleSlug: $titleSlug) { questionId questionFrontendId codeSnippets { lang langSlug code } envInfo enableRunCode } }",
-        "variables": {"titleSlug": title_slug}}
+def generate_python3_file_content(code_snippet: str, test_cases: list[str]) -> str:
+    function_name = re.search(r"(?<=class Solution:\n {4}def )[^(]+", code_snippet).group()
 
-    api_url = "https://leetcode.com/graphql/"
+    file_content = f"{code_snippet}raise NotImplementedError\n\n\nif __name__ == \"__main__\":\n"
 
-    data_info = requests.get(api_url, json=json_query_info).json()
-    data_info_question = data_info["data"]["question"]
+    for test_case in test_cases:
+        args = test_case.split('\n')
+        file_content += f"    print(Solution().{function_name}({', '.join(args)}))\n"
 
-    problem_id = data_info_question["questionFrontendId"]
-    problem_title = data_info_question["questionTitle"]
-    example_test_cases = data_info_question["exampleTestcaseList"]
+    return file_content
 
-    data_code_snippets = requests.get(api_url, json=json_query_code_snippets).json()
-    code_snippets = data_code_snippets["data"]["question"]["codeSnippets"]
 
-    return problem_id, problem_title, example_test_cases, code_snippets
+def generate_rust_file_content(code_snippet: str, test_cases: list[str]) -> str:
+    function_name = re.search(r"(?<=impl Solution {\n {4}pub fn )[^(]+", code_snippet).group()
+
+    file_content = f"struct Solution;\n\n{code_snippet}\n\nfn main() {{\n"
+
+    for test_case in test_cases:
+        args = test_case.split('\n')
+        file_content += f"    println!(\"{{:?}}\", Solution::{function_name}({', '.join(args)}));\n"
+
+    file_content += "}"
+
+    return file_content
+
+
+def create_file(file_path: Path, content: str) -> None:
+    os.makedirs(file_path.parent, exist_ok=True)
+
+    with open(file_path, 'w') as f:
+        f.write(content)
 
 
 def main() -> None:
-    lang_map = {"c++": "cpp",
-                "cpp": "cpp",
-                "java": "java",
-                "python": "python",
-                "py": "python",
-                "python3": "python3",
-                "py3": "python3",
-                "c": "c",
-                "c#": "csharp",
-                "cs": "csharp",
-                "csharp": "csharp",
-                "javascript": "javascript",
-                "js": "javascript",
-                "ruby": "ruby",
-                "rb": "ruby",
-                "swift": "swift",
-                "go": "golang",
-                "golang": "golang",
-                "scala": "scala",
-                "kotlin": "kotlin",
-                "kt": "kotlin",
-                "rust": "rust",
-                "rs": "rust",
-                "php": "php",
-                "typescript": "typescript",
-                "ts": "typescript",
-                "racket": "racket",
-                "rkt": "racket",
-                "dart": "dart"}
-
-    lang_file_extension = {"cpp": ".cpp",
-                           "java": ".java",
-                           "python": ".py",
-                           "python3": ".py",
-                           "c": ".c",
-                           "csharp": ".cs",
-                           "javascript": ".js",
-                           "ruby": ".rb",
-                           "swift": ".swift",
-                           "golang": ".go",
-                           "scala": ".scala",
-                           "kotlin": ".kt",
-                           "rust": ".rs",
-                           "php": ".php",
-                           "typescript": ".ts",
-                           "racket": ".rkt",
-                           "dart": ".dart"}
-
     problem_url = input("Enter problem url: ")
-    lang = lang_map[input("Enter programming language (default is python3): ").lower() or "python3"]
 
-    problem_id, problem_title, example_test_cases, code_snippets = get_problem_data(problem_url)
+    problem_data = get_problem_data(problem_url)
+    problem_id = problem_data["questionFrontendId"]
+    problem_title = problem_data["questionTitle"]
+    test_cases = problem_data["exampleTestcaseList"]
 
-    file_name = f"{problem_id}. {problem_title}{lang_file_extension[lang]}"
-    directory = f"./{lang}/"
-    file_path = directory + file_name
+    language = input("Enter programming language (default is python3): ").lower() or "python3"
+    language_slug = PROGRAMMING_LANGUAGES[language]
+
+    code_snippet = next(
+        (snippet["code"] for snippet in problem_data["codeSnippets"] if snippet["langSlug"] == language_slug),
+        ""
+    )
+
+    directory = Path(language_slug).resolve()
+
+    file_name = f"{problem_id}. {problem_title}{LANGUAGE_FILE_EXTENSIONS[language_slug]}"
+    file_path = directory / file_name
 
     if os.path.exists(file_path):
         if input(f"A file already exists at '{file_path}', do you want to overwrite it? [Y/n]: ").lower() != "y":
             print("Exiting program!")
             return
 
-    file_content = ""
+    match language_slug:
+        case "python3":
+            file_content = generate_python3_file_content(code_snippet, test_cases)
+        case "rust":
+            file_content = generate_rust_file_content(code_snippet, test_cases)
+        case _:
+            file_content = code_snippet
 
-    for code_snippet in code_snippets:
-        if code_snippet["langSlug"] == lang:
-            file_content = code_snippet["code"]
-            break
-
-    if lang == "python3":
-        file_content = f'from typing import Dict, List, Optional, Tuple\n\n\n{file_content}pass\n\n\nif __name__ == "__main__":\n    '
-
-        try:
-            function_name = file_content.split('\n')[4].split()[1].split('(')[0]
-        except IndexError:
-            function_name = "replace_this_with_solution_method_name"
-
-        test_cases = []
-
-        for test_case in example_test_cases:
-            args = ", ".join(test_case.split('\n'))
-            test_cases.append(f"print(Solution().{function_name}({args}))")
-
-        file_content += "\n    ".join(test_cases)
-
-    os.makedirs(directory, exist_ok=True)
-
-    with open(file_path, 'w') as f:
-        f.write(file_content)
+    create_file(file_path, file_content)
 
     print(f"File '{file_path}' created successfully!")
 
