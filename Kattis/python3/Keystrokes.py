178339b160d8ca3d1f2048b58bc4e0b2e5e5abe7
Melker Widén <melker.widen@gmail.com>
2023-07-03 23:37:45 +0200

    solution updated

--- Kattis/python3/Keystrokes.py
+++ Kattis/python3/Keystrokes.py
@@ -1,28 +1,56 @@
-# for token in s:
-#     if token in ["L", "R"]:
-#         pos = max(pos + tokens[token], 0)
-#         pos += tokens[token]
-#     elif token == "B":
-#         result_1.pop(pos - 1)
-#         pos = max(pos - 1, 0)
-#         pos -= 1
-#         result_1.pop(pos)
-#     else:
-#         result_1.insert(pos, token)
-#         pos += 1
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import Optional
 
-s = input()
-result = []
-tokens = {"L": -1, "R": 1, "B": -1}
-pos = 0
 
-for token in s:
-    if token in ["L", "R", "B"]:
-        pos += tokens[token]
-        if token == "B":
-            result.pop(pos)
-    else:
-        result.insert(pos, token)
-        pos += 1
+@dataclass
+class DoublyLinkedList:
+    value: str = ''
+    next: Optional[DoublyLinkedList] = None
+    prev: Optional[DoublyLinkedList] = None
 
-print("".join(result))
+
+root = node = DoublyLinkedList()
+
+for token in input():
+    if token == 'L':
+        if node.prev is None:
+            node.prev = DoublyLinkedList(next=node)
+        node = node.prev
+        if node.prev is None:
+            root = node
+        continue
+
+    if token == 'R':
+        node = node.next
+        continue
+
+    if token == 'B':
+        if node.prev is None:
+            node.value = ''
+            continue
+
+        node = node.prev
+        if node.prev is None:
+            root = node
+
+        node.next = node.next.next
+
+        if node.next is not None:
+            node.next.prev = node
+
+        continue
+
+    if node.value == '':
+        node.value = token
+        continue
+
+    new_node = DoublyLinkedList(token, node.next, node)
+    if new_node.next is not None:
+        new_node.next.prev = new_node
+    node.next = new_node
+    node = new_node
+
+while root:
+    print(root.value, end='')
+    root = root.next
