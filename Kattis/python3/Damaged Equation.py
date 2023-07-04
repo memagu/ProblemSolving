b53bc80aeffa86c00de94b1327420949cc2a8ff5
Melker Widén <melker.widen@gmail.com>
2023-07-04 22:17:37 +0200

    solution updated

--- Kattis/python3/Damaged Equation.py
+++ Kattis/python3/Damaged Equation.py
@@ -1,30 +1,60 @@
+# a, b, c, d = map(int, input().split())
+#
+#
+# def calc(x, y):
+#     mul = ["*", x * y]
+#     add = ["+", x + y]
+#     sub = ["-", x - y]
+#
+#     try:
+#         div = ["/", x // y]
+#     except ZeroDivisionError:
+#         div = ["/", None]
+#
+#     return mul, add, sub, div
+#
+#
+# hits = []
+#
+# for i in range(4):
+#     vl = calc(a, b)[i]
+#     for j in range(4):
+#         hl = calc(c, d)[j]
+#         if vl[1] == hl[1] and not ((vl[0] == "/" and b == 0) or (hl[0] == "/" and c == 0)):
+#             hits.append(f"{a} {vl[0]} {b} = {c} {hl[0]} {d}")
+#
+# if len(hits) == 0:
+#     print("problems ahead")
+# else:
+#     for i in range(len(hits)):
+#         print(hits[i])
+
+
+from operator import add, floordiv, mul, sub
+
+operators = (
+    (mul, '*'),
+    (add, '+'),
+    (sub, '-'),
+    (floordiv, '/')
+)
+
 a, b, c, d = map(int, input().split())
 
+solution_found = False
+for op_left, symbol_left in operators:
+    if not b and op_left is floordiv:
+        continue
 
-def calc(x, y):
-    mul = ["*", x * y]
-    add = ["+", x + y]
-    sub = ["-", x - y]
+    for op_right, symbol_right in operators:
+        if not d and op_right is floordiv:
+            continue
 
-    try:
-        div = ["/", x // y]
-    except ZeroDivisionError:
-        div = ["/", None]
+        if op_left(a, b) != op_right(c, d):
+            continue
 
-    return mul, add, sub, div
+        solution_found = True
+        print(f"{a} {symbol_left} {b} = {c} {symbol_right} {d}")
 
-
-hits = []
-
-for i in range(4):
-    vl = calc(a, b)[i]
-    for j in range(4):
-        hl = calc(c, d)[j]
-        if vl[1] == hl[1] and not ((vl[0] == "/" and b == 0) or (hl[0] == "/" and c == 0)):
-            hits.append(f"{a} {vl[0]} {b} = {c} {hl[0]} {d}")
-
-if len(hits) == 0:
+if not solution_found:
     print("problems ahead")
-else:
-    for i in range(len(hits)):
-        print(hits[i])
