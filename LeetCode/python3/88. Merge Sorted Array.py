ac0d661f036f05b149c56257d2da4257b2b93b8a
Melker Widén <melker.widen@gmail.com>
2023-09-25 23:00:43 +0200

    Update solution

--- LeetCode/python3/88. Merge Sorted Array.py
+++ LeetCode/python3/88. Merge Sorted Array.py
@@ -3,19 +3,18 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        i = m - 1
-        j = n - 1
-        fill_pointer = i + n
+        fill_pointer = m + n - 1
+        m -= 1
+        n -= 1
 
-        while j >= 0:
-            if i >= 0 and nums1[i] > nums2[j]:
-                nums1[fill_pointer] = nums1[i]
-                i -= 1
+        while n >= 0:
+            if m >= 0 and nums1[m] > nums2[n]:
+                nums1[fill_pointer] = nums1[m]
+                m -= 1
             else:
-                nums1[fill_pointer] = nums2[j]
-                j -= 1
+                nums1[fill_pointer] = nums2[n]
+                n -= 1
             fill_pointer -= 1
-
 
 if __name__ == "__main__":
     print(Solution().merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3))
