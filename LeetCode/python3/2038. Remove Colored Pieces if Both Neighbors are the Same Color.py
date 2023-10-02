bd4de6a46ab6e1592bbe37144d50cf136c4e9ddf
Melker Widén <melker.widen@gmail.com>
2023-10-02 14:50:48 +0200

    Update solution

--- LeetCode/python3/2038. Remove Colored Pieces if Both Neighbors are the Same Color.py
+++ LeetCode/python3/2038. Remove Colored Pieces if Both Neighbors are the Same Color.py
@@ -4,7 +4,7 @@ class Solution:
         b_triplets = 0
 
         for i in range(1, len(colors) - 1):
-            if not (colors[i-1] == colors[i] == colors[i+1]):
+            if not (colors[i - 1] == colors[i] == colors[i + 1]):
                 continue
 
             if colors[i] == 'A':
@@ -14,7 +14,6 @@ class Solution:
             b_triplets += 1
 
         return a_triplets > b_triplets
-
 
 
 if __name__ == "__main__":
