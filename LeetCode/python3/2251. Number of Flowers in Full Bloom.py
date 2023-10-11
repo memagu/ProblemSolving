4e920c04e3cc1ecf05e7fdc4e119cf7938e5f5ba
Melker Widén <melker.widen@gmail.com>
2023-10-11 21:29:40 +0200

    Add solution

--- LeetCode/python3/2251. Number of Flowers in Full Bloom.py
+++ LeetCode/python3/2251. Number of Flowers in Full Bloom.py
@@ -1,21 +1,13 @@
-from bisect import bisect_left
-from collections import defaultdict
+from bisect import bisect_left, bisect_right
 from operator import itemgetter
 from typing import List
 
-
 class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
-        flower_delta = defaultdict(int)
+        starts = sorted(map(itemgetter(0), flowers))
+        ends = sorted(map(itemgetter(1), flowers))
 
-        for start, end in flowers:
-            flower_delta[start] += 1
-            flower_delta[end + 1] -= 1
-
-        last_value = 0
-        prefix_sum = [(key, last_value := last_value + delta) for key, delta in sorted(flower_delta.items())]
-
-        return [prefix_sum[bisect_left(prefix_sum, time + 1, key=itemgetter(0)) - 1][1] for time in people]
+        return [bisect_right(starts, time) - bisect_left(ends, time) for time in people]
 
 
 if __name__ == "__main__":
