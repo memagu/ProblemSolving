17f2634fca1e2ebd9493d06c2abbf22903db0dd8
Melker Widén <melker.widen@gmail.com>
2023-10-28 00:36:09 +0200

    Update solution

--- LeetCode/python3/41. First Missing Positive.py
+++ LeetCode/python3/41. First Missing Positive.py
@@ -1,19 +1,23 @@
 from typing import List
 
 
-# Make space o(n) -> O(1)
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        unique_nums = set(nums)
+        for i in range(len(nums)):
+            while 0 < nums[i] <= len(nums) and nums[nums[i] - 1] != nums[i]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
 
-        result = 1
-        while result in unique_nums:
-            result += 1
+        for i, num in enumerate(nums, 1):
+            if num != i:
+                return i
 
-        return result
+        return len(nums) + 1
+
 
 
 if __name__ == "__main__":
     print(Solution().firstMissingPositive([1, 2, 0]))
     print(Solution().firstMissingPositive([3, 4, -1, 1]))
     print(Solution().firstMissingPositive([7, 8, 9, 11, 12]))
+    print(Solution().firstMissingPositive([3, 4, 5, -1, 1]))
+    print(Solution().firstMissingPositive([5,1,1,1,1,1,1,5,4,3,2,1,0]))
