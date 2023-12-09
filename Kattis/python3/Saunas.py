7c9da47a43ef9cb34709192f905d56f76d38fed8
Melker Widén <melker.widen@gmail.com>
2023-12-09 03:23:56 +0100

    Update solution

--- Kattis/python3/Saunas.py
+++ Kattis/python3/Saunas.py
@@ -2,5 +2,5 @@ i=input
 v=sorted(list(map(int,i().split()))[::-1]for _ in range(int(i())))
 _,c,b,a=map(sum,zip(*v))
 r=0
-for t,f,e,d in v:r=max(r,*(a*x*x+b*x+c for x in(0,-b/(2*a+1e-9),t)if 0<=x<=t));a-=d;b-=e;c-=f
+for t,f,e,d in v:r=max(r,c,*(a*x*x+b*x+c for x in(-b/(2*a+1e-9),t)if 0<x<=t));a-=d;b-=e;c-=f
 print(r)
