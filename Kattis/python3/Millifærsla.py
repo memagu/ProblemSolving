93795740ee143c0e3d4a445af82052e4ad226213
Melker Widén <melker.widen@gmail.com>
2023-12-13 19:07:32 +0100

    Add solution

---
+++ Kattis/python3/Millifærsla.py
@@ -0,0 +1,8 @@
+a, b, c = map(int, (input() for _ in range(3)))
+smallest = min(a, b, c)
+if smallest == a:
+    print("Monnei")
+elif smallest == b:
+    print("Fjee")
+else:
+    print("Dolladollabilljoll")
