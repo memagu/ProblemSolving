667a4e8dc70b1fba4d047bfbba7b811defc656b2
Melker Widén <melker.widen@gmail.com>
2024-07-06 18:25:59 +0200

    Update solution

--- LeetCode/python3/42. Trapping Rain Water.py
+++ LeetCode/python3/42. Trapping Rain Water.py
@@ -31,28 +31,6 @@ class Solution:
         return result
 
 
-
-"""
-        current = result = 0
-        while current < len(height):
-            water_batch = 0
-            second_tallest = (current + 1, 0, 0)
-            for lookahead in range(current + 1, len(height)):
-                if height[lookahead] >= height[current]:
-                    current = lookahead
-                    break
-
-                water_batch += height[current] - height[lookahead]
-                second_tallest = max(second_tallest, (lookahead, height[lookahead], water_batch - (lookahead - current) * (height[current] - height[lookahead])), key=itemgetter(1))
-            else:
-                result += second_tallest[2]
-                current = second_tallest[0]
-                continue
-            result += water_batch
-            """
-
-
-
 if __name__ == "__main__":
     print(Solution().trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
     print(Solution().trap([4, 2, 0, 3, 2, 5]))
