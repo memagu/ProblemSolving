d8d6b484f45ba326a5f69bbc7a8a3e0b5c0a6fb7
Melker Widén <melker.widen@gmail.com>
2024-12-07 02:31:01 +0100

    Fix error in snippet

--- Advent of Code/snippets/script.scala
+++ Advent of Code/snippets/script.scala
@@ -1,13 +1,13 @@
 //> using scala 3.5
 
 object Solution:
-def part1(file: String): Int =
-  scala.io.Source.fromFile(file)
-  0
+  def part1(file: String): Int =
+    scala.io.Source.fromFile(file)
+    0
 
-def part2(file: String): Int =
-  scala.io.Source.fromFile(file)
-  0
+  def part2(file: String): Int =
+    scala.io.Source.fromFile(file)
+    0
 
 @main
 def main(): Unit =
