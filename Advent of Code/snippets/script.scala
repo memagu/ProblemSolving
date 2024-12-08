b4209b0080ab219e8dd409255649246a3767efd1
Melker Widén <melker.widen@gmail.com>
2024-12-08 04:16:29 +0100

    Restore to previous method

--- Advent of Code/snippets/script.scala
+++ Advent of Code/snippets/script.scala
@@ -1,21 +1,22 @@
 //> using scala 3.5
 
 object Solution:
-  def part1(input: BufferedSource): Int =
+  def part1(file: String): Int =
+    io.Source.fromFile("./data.in")
     0
 
-  def part2(input: BufferedSource): Int =
+  def part2(file: String): Int =
+    io.Source.fromFile("./data.in")
     0
 
 @main
 def main(): Unit =
   import Solution._
 
-  val dataInput: BufferedSource = io.Source.fromFile("./data.in")
-  val exampleInput: BufferedSource = io.Source.fromFile("./example.in")
+  val dataPath: String = "./data.in"
+  val examplePath: String = "./example.in"
 
-  println(s"Part 1: ${part1(dataInput)}")
-  println(s"Part 1 (example): ${part1(exampleInput)}")
-  println(s"Part 2: ${part2(dataInput)}")
-  println(s"Part 2 (example): ${part2(exampleInput)}")
-
+  println(s"Part 1: ${part1(dataPath)}")
+  println(s"Part 1 (example): ${part1(examplePath)}")
+  println(s"Part 2: ${part2(dataPath)}")
+  println(s"Part 2 (example): ${part2(examplePath)}")
