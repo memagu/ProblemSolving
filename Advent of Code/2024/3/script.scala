2004b2e375d83064b3d8bca7aad7787cce66ad6f
Melker Widén <melker.widen@gmail.com>
2024-12-08 02:03:55 +0100

    Add solution

--- Advent of Code/2024/3/script.scala
+++ Advent of Code/2024/3/script.scala
@@ -4,10 +4,25 @@ object Solution:
   def part1(file: String): Int =
     raw"mul\((\d{1,3}),(\d{1,3})\)".r.findAllMatchIn(scala.io.Source.fromFile(file).mkString).map(patternMatch => patternMatch.group(1).toInt * patternMatch.group(2).toInt).sum
 
-  // Unfinised
   def part2(file: String): Int =
-    raw"mul\((\d{1,3}),(\d{1,3})\)".r.findAllMatchIn(scala.io.Source.fromFile(file).mkString.replaceAll(raw"don't\(\).*?do\(\)", "").reverse.replaceFirst(raw".*\)\(t'nod", "").reverse).map(patternMatch => patternMatch.group(1).toInt * patternMatch.group(2).toInt).sum
+    import scala.util.matching.Regex
 
+    val input: String = scala.io.Source.fromFile(file).mkString
+    var multiplicationEnabled: Boolean = true
+    var result: Int = 0
+
+    val mulPattern: Regex = """^mul\((\d{1,3}),(\d{1,3})\)[\S\s]*$""".r
+    val doPattern: Regex = """^do\(\)[\S\s]*$""".r
+    val dontPattern: Regex = """^don't\(\)[\S\s]*$""".r
+
+    for i <- input.indices do
+      input.substring(i) match
+        case mulPattern(a, b) if multiplicationEnabled => result += a.toInt * b.toInt
+        case doPattern() => multiplicationEnabled = true
+        case dontPattern() => multiplicationEnabled = false
+        case _ =>
+
+    result
 
 @main
 def main(): Unit =
