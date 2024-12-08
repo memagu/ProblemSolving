76f6cf0968457c1150a3be59d0c808a3d7d47701
Melker Widén <melker.widen@gmail.com>
2024-12-08 04:03:18 +0100

    Update scala snippet

--- Advent of Code/snippets/script.scala
+++ Advent of Code/snippets/script.scala
@@ -1,23 +1,21 @@
 //> using scala 3.5
 
 object Solution:
-  def part1(file: String): Int =
-    scala.io.Source.fromFile(file)
+  def part1(input: String): Int =
     0
 
-  def part2(file: String): Int =
-    scala.io.Source.fromFile(file)
+  def part2(input: String): Int =
     0
 
 @main
 def main(): Unit =
   import Solution._
 
-  val dataPath: String = "./data.in"
-  val examplePath: String = "./example.in"
+  val dataInput: String = io.Source.fromFile("./data.in").mkString
+  val exampleInput: String = io.Source.fromFile("./example.in").mkString
 
-  println(s"Part 1: ${part1(dataPath)}")
-  println(s"Part 1 (example): ${part1(examplePath)}")
-  println(s"Part 2: ${part2(dataPath)}")
-  println(s"Part 2 (example): ${part2(examplePath)}")
+  println(s"Part 1: ${part1(dataInput)}")
+  println(s"Part 1 (example): ${part1(exampleInput)}")
+  println(s"Part 2: ${part2(dataInput)}")
+  println(s"Part 2 (example): ${part2(exampleInput)}")
 
